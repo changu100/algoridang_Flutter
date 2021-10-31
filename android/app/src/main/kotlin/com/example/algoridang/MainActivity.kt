9c61d525bab1bed1691b8f5fa83443c4a6ab5004
changu100 <blue_rose17@naver.com>
2021-11-01 02:12:26 +0900

    기본 알고리당 수행

---
+++ android/app/src/main/kotlin/com/example/algoridang/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.algoridang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+//플러터(Flutter) - VSCode에서 gradle sync 하기
+//https://blog.naver.com/chandong83/222063184403
+//.\gradlew --refresh-dependencies
+
+//BUG! exception in phase 'semantic analysis' in source unit '_BuildScript_' Unsupported class file major version 60
+// https://velog.io/@juunini/react-native-%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EB%B9%8C%EB%93%9C%EA%B0%80-%EC%95%88%EB%90%A0-%EB%95%8C-Unsupported-class-file-major-version-60
+
+//build test
+//flutter build apk --release --target-platform=android-arm64 --no-sound-null-safety
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+        body: Container(
+      margin: EdgeInsets.only(top: statusBarHeight),
+      child: WebView(
+        initialUrl: "https://www.algoridang.com/",
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    ));
+  }
+}
